57d4e35e52aac3b63b6a6a3d796befc0dfd65569
Jeancarlo Javier <jeancarlojavier43@gmail.com>
2024-07-04 18:44:42 -0500

    Local Dev apiURL

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,12 +1,10 @@
 import type { Post, PostCreate, UnprocessedPost } from '../types/postTypes'
 import { getLocalAuth } from './users'
 
-// const apiUrl =
-//   import.meta.env.VITE_ENV === 'development'
-//     ? '/api'
-//     : import.meta.env.VITE_BACKEND_URL
-const apiUrl = import.meta.env.VITE_BACKEND_URL
-// console.log(apiUrl)
+const apiUrl =
+  import.meta.env.VITE_ENV === 'development'
+    ? '/api'
+    : import.meta.env.VITE_BACKEND_URL
 
 export async function fetchPosts(): Promise<Post[] | null> {
   const response = await fetch(`${apiUrl}/posts`, {
